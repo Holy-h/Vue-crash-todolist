cac960840437688e49368558753cc2870c2ab5da
Bonggt <kepy1106@gmail.com>
2020-05-17 16:17:34 +0900

    Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,17 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
 export default {
   name: "App",
-  components: { Todos, Header, AddTodo },
-  data() {
-    return {
-      todos: []
-    };
+  components: {
+    Header,
   },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then((this.todos = this.todos.filter(todo => todo.id !== id)))
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed
-        })
-        .then(res => (this.todos = [...this.todos, res.data]))
-        .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
-  }
 };
 </script>
 
@@ -51,6 +20,7 @@ export default {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  font-size: 16px;
 }
 body {
   font-family: Arial, Helvetica, sans-serif;
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>이 페이지는 TodoList의 About페이지 입니다. v1.0.0</p>
+  </div>
+</template>
